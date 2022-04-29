0a19c648589134fb5d5d409a7a59e33484e42c6d
Gianluca Paris <gparis@datakitchen.io>
2022-04-29 16:56:03 +0200

    feat(core): add async names validator

--- libs/core/src/lib/utilities/validators.ts
+++ libs/core/src/lib/utilities/validators.ts
@@ -1,9 +1,25 @@
 import { AbstractControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export class CustomValidators {
   static forbiddenNames(names: string[]) {
     return (control: AbstractControl) => {
       return !control.value ? null : names.map(name => name.toLowerCase()).includes(control.value.toLowerCase()) ? { nameExists: true } : null;
+    };
+  }
+
+  static forbiddenNamesAsync(observable: Observable<any[]>) {
+    return (control: AbstractControl): Observable<{ [key: string]: any } | null> => {
+      return observable.pipe(
+        map(names => {
+          if (names.includes(control?.value)) {
+            return { nameExists: true };
+          }
+
+          return null;
+        })
+      );
     };
   }
 }
