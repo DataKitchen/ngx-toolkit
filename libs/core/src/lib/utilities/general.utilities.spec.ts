73f71466080bc6fb802346a19598e1423bbaed8c
Gianluca Paris <gparis@datakitchen.io>
2022-09-07 15:57:47 +0200

    test(core): add tests for pick, omit, isObject utilities

--- libs/core/src/lib/utilities/general.utilities.spec.ts
+++ libs/core/src/lib/utilities/general.utilities.spec.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { caseInsensitiveIncludes, isValidDate, parseDate } from './general.utilities';
+=======
+import { caseInsensitiveIncludes, isObject, omit, parseDate, pick } from './general.utilities';
+>>>>>>> f6ff16e (test(core): add tests for pick, omit, isObject utilities)
 
 describe('general-utilities', () => {
 
@@ -44,6 +48,7 @@ describe('general-utilities', () => {
     });
   });
 
+<<<<<<< HEAD
   describe('isValidDate', () => {
     it('should return true if a valid date is passed in input', () => {
       expect(isValidDate(new Date())).toBeTruthy();
@@ -51,6 +56,45 @@ describe('general-utilities', () => {
 
     it('should return false if no valid date is passed in input', () => {
       expect(isValidDate(3)).toBeFalsy();
+=======
+  describe('isObject', () => {
+    it('should return true if the input value is an object', () => {
+      const input = { test: 'test' };
+      expect(isObject(input)).toBeTruthy();
+    });
+
+    it('should return true if the input value is a function', () => {
+      const input = () => {
+        return 'test';
+      };
+      expect(isObject(input)).toBeTruthy();
+    });
+
+    it('should return false if the input is null', () => {
+      const input = null;
+      expect(isObject(input)).toBeFalsy();
+    });
+
+    it('should return false if the input is not an object or a function', () => {
+      const input = 'test';
+      expect(isObject(input)).toBeFalsy();
+    });
+  });
+
+  describe('omit', () => {
+    it('should omit specified properties from input object', () => {
+      const input = { name: 'Test', surname: 'Utility', age: 30 };
+      const expected = { age: 30 };
+      expect(omit(input, [ 'name', 'surname' ])).toEqual(expected);
+    });
+  });
+
+  describe('pick', () => {
+    it('should pick only specified properties from input object', () => {
+      const input = { name: 'Test', surname: 'Utility', age: 30 };
+      const expected = { age: 30 };
+      expect(pick(input, [ 'age' ])).toEqual(expected);
+>>>>>>> f6ff16e (test(core): add tests for pick, omit, isObject utilities)
     });
   });
 });
