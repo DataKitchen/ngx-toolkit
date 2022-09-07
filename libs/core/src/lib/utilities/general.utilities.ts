d06c75632b3d0071855aed64f938ecef988845e0
Gianluca Paris <gparis@datakitchen.io>
2022-09-07 15:57:28 +0200

    feat(core): add pick, omit, isObject utilities

--- libs/core/src/lib/utilities/general.utilities.ts
+++ libs/core/src/lib/utilities/general.utilities.ts
@@ -13,6 +13,28 @@ export function parseDate(date: string): Date {
   return new Date(date + '+00:00');
 }
 
+<<<<<<< HEAD
 export function isValidDate(d: any) {
   return d instanceof Date && !isNaN(d as any);
+=======
+export function isObject(value: any): boolean {
+  const type = typeof value;
+  return value != null && (type === 'object' || type === 'function');
+}
+
+export function omit(obj: any, keys: string[]) {
+  const newObject: any = {};
+  Object.entries(obj).filter(([ k ]) => !keys.includes(k)).forEach(([ key, value ]) => {
+    newObject[key] = value;
+  });
+  return newObject;
+}
+
+export function pick(obj: any, keys: string[]) {
+  const newObject: any = {};
+  Object.entries(obj).filter(([ k ]) => keys.includes(k)).forEach(([ key, value ]) => {
+    newObject[key] = value;
+  });
+  return newObject;
+>>>>>>> 38eebec (feat(core): add pick, omit, isObject utilities)
 }
