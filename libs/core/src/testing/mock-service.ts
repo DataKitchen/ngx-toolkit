d0cdc76745c9f3ab6954073d645cc44bec913add
Davide Cavaliere <dcavaliere@datakitchen.io>
2022-09-09 16:13:45 +0200

    refactor(mock-service): refactor code to make mocker scan for all parent classes
    this also fix an issue for the mocker was failing to recognize getter functions

--- libs/core/src/testing/mock-service.ts
+++ libs/core/src/testing/mock-service.ts
@@ -1,36 +1,37 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Class } from 'utility-types';
+
+const builtInMethods = Object.keys(Object.getOwnPropertyDescriptors(Object.getPrototypeOf({})));
+
 export type Mocked<T> = {
   [M in keyof T]: jest.Mock & T[M]
 } & T;
 
 export function MockService<T extends new(...args: any[]) => any>(service: T): (klass?: Class<any>) => Mocked<T> {
 
-  const proto = service.prototype as unknown;
-  const methods = Object.getOwnPropertyNames(proto)
-    .filter((k) => {
-      // @ts-ignore
-      return typeof proto[k] === 'function';
-    })
-    .filter((k) => {
-      return k !== 'constructor';
-    });
+  let proto = service.prototype as unknown;
 
-  const parentProto = Object.getPrototypeOf(proto) as unknown;
-  const parentMethods = Object.getOwnPropertyNames(parentProto)
-    .filter((k) => {
-      // @ts-ignore
-      return typeof proto[k] === 'function';
-    })
-    .filter((k) => {
-      return k !== 'constructor';
-    });
+  const methods: string[] = [];
 
-  return (_klass = class {
-  }): Mocked<T> => {
+  while (proto) {
+    const ownMethods = Object.keys(Object.getOwnPropertyDescriptors(proto))
+      .filter((k) => {
+        return !builtInMethods.includes(k);
+      });
+
+    methods.push(...ownMethods);
+
+    try {
+      proto = Object.getPrototypeOf(proto);
+    } catch (e) {
+      proto = null;
+    }
+  }
+
+  return (_klass = class {}): Mocked<T> => {
     const mockedClass = _klass;
 
-    for (const method of [ ...methods, ...parentMethods ]) {
+    for (const method of methods) {
       // @ts-ignore
       (mockedClass.prototype as unknown)[method] = jest.fn();
     }
@@ -39,10 +40,9 @@ export function MockService<T extends new(...args: any[]) => any>(service: T): (
   };
 }
 
-// @ts-ignore
-export function MockProvider(klass, overrides) {
+export function MockProvider(klass: Class<any>, overrides: Class<any> = class {}) {
   return {
     provide: klass,
     useClass: MockService(klass)(overrides)
-  }
+  };
 }
