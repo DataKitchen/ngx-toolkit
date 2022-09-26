c66c28eb0ba083e016d6ace2ca88a2f1ef019936
Gianluca Paris <gparis@datakitchen.io>
2022-09-26 11:42:52 +0200

    fix(core): change type guard to support both onPageChange and pageChange$ approaches

--- core-component/with-table.ts
+++ core-component/with-table.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Entity } from '../../entity';
@@ -15,29 +15,31 @@ export interface Pagination {
 }
 
 export const defaultPagination: Pagination & SortOptions<Entity> = {
-  page: 1,
+  page: 0,
   count: 10,
   // sort_by: 'name',
   // sort: 'asc',
 };
 
 export interface WithTable {
-    paginator: MatPaginator;
+  paginator: MatPaginator;
 
-    pageChange$?: BehaviorSubject<Pagination>;
+  pageChange$?: BehaviorSubject<Pagination>;
 
-    sortBy?: MatSort;
-    onPageChange: (page: Pagination) => void;
+  sortBy?: MatSort;
+
+  // Implementing this lifecycle is no longer mandatory, components that
+  // implement this interface can either use `onPageChange` or `pageChange$`
+  onPageChange?: (page: Pagination) => void;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export class WithTable {}
+// export class WithTable {}
 
 export function isWithTable(c: unknown): c is WithTable {
-    return typeof (c as { [key: string]: any; })['onPageChange'] === 'function';
+    return typeof (c as { [key: string]: any; })['onPageChange'] === 'function' || (c as { [key: string]: any; })['pageChange$'] instanceof Subject;
 }
 
 export function isPagination(c: unknown): c is Pagination {
-    return typeof c === 'object' && c !== null && c !== undefined && 'page' in c;
+  return typeof c === 'object' && c !== null && c !== undefined && 'page' in c;
 }
-
