3a333f45fc3fa7c2a1f888657be928d610af52f3
Davide Cavaliere <dcavaliere@datakitchen.io>
2022-11-17 10:52:44 +0100

    chore: move Pagination and SortOptions interface to entity.model file

--- core-component/with-table.ts
+++ core-component/with-table.ts
@@ -1,23 +1,6 @@
 import { Subject } from 'rxjs';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
-
-// TODO move SortOptions and Pagination to Entity
-export interface SortOptions {
-  sort_by?: any;
-  sort?: 'asc' | 'desc' | '';
-}
-
-export interface Pagination extends SortOptions{
-  page?: number;
-  count?: number;
-}
-
-export function isPagination(c: unknown): c is Pagination {
-  return typeof c === 'object' && c !== null && c !== undefined && 'page' in c;
-}
-
-
 export interface WithTable {
   paginator: MatPaginator;
   __pageChange$?: Subject<PageEvent>;
