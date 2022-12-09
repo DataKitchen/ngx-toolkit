0c05df9914b7730bce0bd4f181abb176b6f1d157
Davide Cavaliere <dcavaliere@datakitchen.io>
2022-12-09 18:33:02 +0100

    refactor: replace references to `expectObservableWithCallback`
    also fixing some issues on dag.component.spec, namely:
    - `fixture.detectChanges()` should have been called after replacing
    private fields with mocks
    - calling manually `ngAfterViewChanges()` was problematic creating too
    many subscriptions
    - there are still some tests failing which have been e`x`cluded for now

--- libs/core/src/testing/expect-observable.ts
+++ libs/core/src/testing/expect-observable.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/ban-types */
 import { Observable } from 'rxjs';
-import { RunHelpers, TestScheduler } from 'rxjs/testing';
+import { TestScheduler } from './test-scheduler';
 
 interface ExpectObservableMatcher<T> {
   toBe: (marbles: string, values?: { [k: string]: T }, errorValue?: any) => void;
@@ -8,48 +8,27 @@ interface ExpectObservableMatcher<T> {
   toContain: (...vales: Array<T>) => void;
 }
 
-class MyTestScheduler extends TestScheduler {
-  useContain = false;
-
-  constructor() {
-    super((actual, expected) => {
-      if (this.useContain) {
-        for (const expectedElement of expected) {
-          expect(actual).toContainEqual(expectedElement);
-        }
-      } else {
-        expect(actual).toEqual(expected);
-      }
-    });
-  }
-}
-
-export const testScheduler = new MyTestScheduler();
-
-export const getTestScheduler = (useContain = false) => {
-  testScheduler.useContain = useContain;
-  return testScheduler;
-};
-
-export function expectObservableWithCallback<T>(callback: (helpers: RunHelpers) => T): T {
-  return getTestScheduler().run(callback);
-}
-
+/**
+ * @deprecated use new TestScheduler().expect$
+ * @param obs
+ */
 export function expectObservable<
   K extends Observable<any>,
   T = K extends Observable<infer A> ? A : never>(obs: K): ExpectObservableMatcher<T> {
+
+  const testScheduler = new TestScheduler();
 
   const alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
   return {
     toBe: (marble: string, values?: {}, errorValue?: any) => {
-      getTestScheduler().run(({ expectObservable }) => {
+      testScheduler.run(({ expectObservable }) => {
         expectObservable(obs).toBe(marble, values, errorValue);
       });
     },
     toEqual: (...values: Array<{}>) => {
 
-      getTestScheduler().run(({ expectObservable }) => {
+      testScheduler.run(({ expectObservable }) => {
 
         const marble = values.map((_, index) => {
           return alphabet[index];
@@ -66,22 +45,7 @@ export function expectObservable<
     },
     toContain: (...values: Array<{}>) => {
 
-      getTestScheduler(true).run(({ expectObservable }) => {
-
-        const marble = values.map((_, index) => {
-          return alphabet[index];
-        });
-
-        const v = values.reduce((acc, val, index) => {
-          // @ts-ignore
-          acc[alphabet[index]] = val;
-          return acc;
-        }, {});
-
-
-        expectObservable(obs).toBe(marble.join(''), v);
-
-      });
+      testScheduler.expect$(obs).toContain(...values);
     }
   };
 }
