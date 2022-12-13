c1e0a8f67b2b67f6c000676b7382485d1e47e0e4
Davide Cavaliere <dcavaliere@datakitchen.io>
2022-12-13 10:10:33 +0100

    refactor(entity): sortable and searchable utility types belong to entity

--- core-component/has-search-form/has-search-form.ts
+++ core-component/has-search-form/has-search-form.ts
@@ -1,38 +1,6 @@
 import { TypedFormGroup } from '../../../typed-form/typed-forms';
-import { $Keys, PickByValue } from 'utility-types';
 import { AbstractControl } from '@angular/forms';
 import { Subject } from 'rxjs';
-
-
-export type FieldType = string | number | boolean | symbol;
-
-export type SortableField<T extends FieldType> = T & {
-  __sortable: true;
-};
-
-export type SearchableField<T extends FieldType> = T & {
-  __searchable: true;
-};
-
-export type SortableAndSearchable<T extends FieldType> = T & {
-  __sortable__searchable: true;
-}
-
-export type GetSortableFields<T> = $Keys<PickByValue<T, SortableField<FieldType> | SortableAndSearchable<FieldType>>>;
-
-export type GetSearchableFields<T> = PickByValue<T, SearchableField<FieldType> | SortableAndSearchable<FieldType>>;
-
-export type Rebrand<T> = {
-  [k in keyof T]: T[k] extends SortableField<infer R> ? R :
-    T[k] extends SortableAndSearchable<infer R> ? R :
-      T[k] extends SearchableField<infer R> ? R : T[k]
-};
-
-export function make<T extends FieldType>(v: T): SearchableField<T> & SortableField<T> & SortableAndSearchable<T> {
-  return v as SearchableField<T> & SortableField<T> & SortableAndSearchable<T>;
-}
-
-export type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>;
 
 export interface HasSearchForm<E extends { [key: string]: any }> {
   search: TypedFormGroup<E>;
