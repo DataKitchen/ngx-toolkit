4a613213cda483f0cfe9ab36b3623e1f2e6887b1
Davide Cavaliere <dcavaliere@datakitchen.io>
2023-04-24 17:29:17 +0200

    chore: [nx migration] migration-v15

--- core-component/has-paginator/has-paginator.ts
+++ core-component/has-paginator/has-paginator.ts
@@ -1,5 +1,5 @@
 import { Subject } from 'rxjs';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatLegacyPaginator as MatPaginator, LegacyPageEvent as PageEvent } from '@angular/material/legacy-paginator';
 
 export interface HasPaginator {
   paginator: MatPaginator;
