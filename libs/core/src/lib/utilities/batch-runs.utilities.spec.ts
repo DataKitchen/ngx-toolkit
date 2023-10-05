3a2e2f6f2d9e741cf89d3947ef423f527ff32f65
Gianluca Paris <gparis@datakitchen.io>
2023-10-05 21:02:08 +0200

    fix(utilities): put dataset utilities in their own file

--- libs/core/src/lib/utilities/batch-runs.utilities.spec.ts
+++ libs/core/src/lib/utilities/batch-runs.utilities.spec.ts
@@ -1,6 +1,6 @@
 import { Run, RunProcessedStatus } from '../models/runs.model';
-import { getDatasetStatus, mostImportantStatus, runsByComponent } from './batch-runs.utilities';
-import { EventType, Schedule, TestStatus } from '../models';
+import { mostImportantStatus, runsByComponent } from './batch-runs.utilities';
+import { TestStatus } from '../models';
 
 describe('batch-runs.utilities', () => {
   const runs = [
@@ -75,104 +75,6 @@ describe('batch-runs.utilities', () => {
 
     it('should give least priority to passed status', () => {
       expect(mostImportantStatus(tests.slice(0, -2))).toBe(RunProcessedStatus.Completed);
-    });
-  });
-
-  describe('getDatasetStatus', () => {
-    it('should return Failed if at least one test is Failed', () => {
-      const datasetOperationEvents: EventType[] = [
-        {
-          raw_data: {
-            operation: 'WRITE'
-          }
-        } as EventType
-      ];
-
-      const tests: { status: TestStatus }[] = [
-        { status: TestStatus.Failed }
-      ];
-
-      const schedules: Schedule[] = [];
-
-      expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.Failed);
-    });
-
-    it('should return CompletedWithWarnings if at least one test is Warning', () => {
-      const datasetOperationEvents: EventType[] = [
-        {
-          raw_data: {
-            operation: 'WRITE'
-          }
-        } as EventType
-      ];
-
-      const tests: { status: TestStatus }[] = [
-        { status: TestStatus.Warning }
-      ];
-
-      const schedules: Schedule[] = [];
-
-      expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.CompletedWithWarnings);
-    });
-
-    describe('if there are no Failed or Warning tests and there is at least one schedule', () => {
-      it('should return completed if there is at least one WRITE operation', () => {
-        const datasetOperationEvents: EventType[] = [
-          {
-            raw_data: {
-              operation: 'WRITE'
-            }
-          } as EventType
-        ];
-        const tests: { status: TestStatus }[] = [ { status: TestStatus.Passed } ];
-        const schedules: Schedule[] = [ {} as Schedule ];
-
-        expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.Completed);
-      });
-
-      it('should return missing if there are no WRITE operations', () => {
-        const datasetOperationEvents: EventType[] = [
-          {
-            raw_data: {
-              operation: 'READ'
-            }
-          } as EventType
-        ];
-        const tests: { status: TestStatus }[] = [ { status: TestStatus.Passed } ];
-        const schedules: Schedule[] = [ {} as Schedule ];
-
-        expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.Missing);
-      });
-    });
-
-    describe('if there are no Failed or Warning tests and there are no schedules', () => {
-      it('should return completed if there is at least one WRITE operation', () => {
-        const datasetOperationEvents: EventType[] = [
-          {
-            raw_data: {
-              operation: 'WRITE'
-            }
-          } as EventType
-        ];
-        const tests: { status: TestStatus }[] = [ { status: TestStatus.Passed } ];
-        const schedules: Schedule[] = [];
-
-        expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.Completed);
-      });
-
-      it('should return pending if there are no WRITE operations', () => {
-        const datasetOperationEvents: EventType[] = [
-          {
-            raw_data: {
-              operation: 'READ'
-            }
-          } as EventType
-        ];
-        const tests: { status: TestStatus }[] = [ { status: TestStatus.Passed } ];
-        const schedules: Schedule[] = [];
-
-        expect(getDatasetStatus(datasetOperationEvents, tests, schedules)).toEqual(RunProcessedStatus.Pending);
-      });
     });
   });
 });
