46eced42fb64fb1d7749d76763994d35ee0abe67
Gianluca Paris <gparis@datakitchen.io>
2023-10-05 21:02:08 +0200

    fix(utilities): put dataset utilities in their own file

--- libs/core/src/lib/utilities/batch-runs.utilities.ts
+++ libs/core/src/lib/utilities/batch-runs.utilities.ts
@@ -1,5 +1,5 @@
 import { Run, RunProcessedStatus } from '../models/runs.model';
-import { DatasetOperationEventData, EventType, Schedule, TestStatus } from '../models';
+import { TestStatus } from '../models';
 
 interface RunsByComponent {
   [componentId: string]: Run[];
@@ -26,36 +26,7 @@ export function mostImportantStatus(runsOrTests: {
     .sort((a, b) => statusWeight[b] - statusWeight[a])[0] ?? initial);
 }
 
-export function getDatasetStatus(datasetOperationEvents: EventType[], tests: {
-  status: TestStatus
-}[], schedules: Schedule[]): RunProcessedStatus {
-  const sortedTests = tests.map(t => t.status).sort((a, b) => statusWeight[b] - statusWeight[a]);
-
-  // If at least 1 Failed or Warning, return Failed or Warning
-  if (sortedTests.length > 0 && sortedTests[0] === TestStatus.Failed || sortedTests[0] === TestStatus.Warning) {
-    return getRunProcessedStatus(sortedTests[0]);
-  }
-
-  // If expectations are set
-  if (schedules.length > 0) {
-    // If at least one write operation
-    if (datasetOperationEvents.find(e => (e.raw_data as DatasetOperationEventData).operation === 'WRITE') !== undefined) {
-      return RunProcessedStatus.Completed;
-    } else {
-      return RunProcessedStatus.Missing;
-    }
-  } else {
-    // If expectation are not set
-    // If at least one write operation
-    if (datasetOperationEvents.find(e => (e.raw_data as DatasetOperationEventData).operation === 'WRITE') !== undefined) {
-      return RunProcessedStatus.Completed;
-    } else {
-      return RunProcessedStatus.Pending;
-    }
-  }
-}
-
-function getRunProcessedStatus(status: RunProcessedStatus | TestStatus): RunProcessedStatus {
+export function getRunProcessedStatus(status: RunProcessedStatus | TestStatus): RunProcessedStatus {
   switch (status) {
     case TestStatus.Passed:
       return RunProcessedStatus.Completed;
