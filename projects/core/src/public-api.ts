02a51b8e57049281c9520a009ddf1733e5752350
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-01-18 18:01:57 +0100

    chore(core): add missing deps and fix export path

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,12 +1,12 @@
 /*
  * Public API Surface of core
  */
-export * from './core.component';
-export * from './rxjs-scheduler.token';
-export * from './has-paginator/has-paginator';
-export * from './has-search-form/has-search-form';
-export * from './decorators/bind-to-query-params/bind-to-query-params';
-export * from './decorators/persist-on-local-storage/persist-on-local-storage';
-export * from './decorators/deferred-props';
-export * from './decorators/memoize/memoize';
-export { HasSorting } from './has-sorting/has-sorting';
+export * from './lib/core.component';
+export * from './lib/rxjs-scheduler.token';
+export * from './lib/has-paginator/has-paginator';
+export * from './lib/has-search-form/has-search-form';
+export * from './lib/decorators/bind-to-query-params/bind-to-query-params';
+export * from './lib/decorators/persist-on-local-storage/persist-on-local-storage';
+export * from './lib/decorators/deferred-props';
+export * from './lib/decorators/memoize/memoize';
+export * from './lib/has-sorting/has-sorting';
