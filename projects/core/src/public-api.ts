ecba418b8b1872599bc76d0528c095eec7951259
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-01-18 17:39:48 +0100

    chore(core): move files to proper folder

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,7 +1,12 @@
 /*
  * Public API Surface of core
  */
-
-export * from './lib/core.service';
-export * from './lib/core.component';
-export * from './lib/core.module';
+export * from './core.component';
+export * from './rxjs-scheduler.token';
+export * from './has-paginator/has-paginator';
+export * from './has-search-form/has-search-form';
+export * from './decorators/bind-to-query-params/bind-to-query-params';
+export * from './decorators/persist-on-local-storage/persist-on-local-storage';
+export * from './decorators/deferred-props';
+export * from './decorators/memoize/memoize';
+export { HasSorting } from './has-sorting/has-sorting';
