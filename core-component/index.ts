fe4b25528c87c97d312550c79fbde61928cf75e5
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-01-18 17:39:48 +0100

    chore(core): move files to proper folder

--- core-component/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './core.component';
-export * from './rxjs-scheduler.token';
-export * from './has-paginator/has-paginator';
-export * from './has-search-form/has-search-form';
-export * from './decorators/bind-to-query-params/bind-to-query-params';
-export * from './decorators/persist-on-local-storage/persist-on-local-storage';
-export * from './decorators/deferred-props';
-export * from './decorators/memoize/memoize';
-export { HasSorting } from './has-sorting/has-sorting';
--- projects/core/src/lib/core.component.spec.ts
+++ projects/core/src/lib/core.component.spec.ts
@@ -1,21 +1,56 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CoreComponent } from './core.component';
+import { Subject } from 'rxjs';
+import spyOn = jest.spyOn;
 
-describe('CoreComponent', () => {
-  let component: CoreComponent;
-  let fixture: ComponentFixture<CoreComponent>;
+describe('core-component', () => {
+
+  @Component({
+    selector: 'comp',
+    template: '<h1>my component</h1>'
+  })
+  class TestClassComponent extends CoreComponent {
+
+    private subject$ = new Subject();
+
+    // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+    override ngOnInit() {
+      this.subscriptions.push(this.subject$.subscribe());
+
+      super.ngOnInit();
+    }
+  }
+
+  let component: TestClassComponent;
+  let fixture: ComponentFixture<TestClassComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CoreComponent]
+      declarations: [ TestClassComponent ],
     });
-    fixture = TestBed.createComponent(CoreComponent);
+
+    fixture = TestBed.createComponent(TestClassComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should unsubscribe when component is destroyed', () => {
+    expect(component['subscriptions'].length).toEqual(0);
+
+    component.ngOnInit();
+    expect(component['subscriptions'].length).not.toEqual(0);
+
+    const unsubscribeSpy = spyOn(component['subscriptions'][0], 'unsubscribe');
+
+    component.ngOnDestroy();
+
+    expect(unsubscribeSpy).toHaveBeenCalled();
+  });
+
 });
+
