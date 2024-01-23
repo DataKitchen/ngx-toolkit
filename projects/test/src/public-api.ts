c80cab087234f1627f86e3beca19acb6805a964e
Davide Cavaliere <cavaliere.davide@gmail.com>
2024-01-23 19:20:14 +0100

    chore(test): import testing utils project

---
+++ projects/test/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of test
+ */
+export * from './lib/expect-observable';
+export * from './lib/mock-component';
+export * from './lib/mock-service';
+export * from './lib/activated-route.mock';
+export * from './lib/test-scheduler';
