ede9c0e9aca3b7c3e938c2d80466db2c50e28403
Davide Cavaliere <dcavaliere@datakitchen.io>
2024-01-24 20:10:05 +0100

    chore(ngx-monaco-editor): migrate project

---
+++ projects/ngx-monaco-editor/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-monaco-editor
+ */
+
+export * from './lib/ngx-monaco-editor.service';
+export * from './lib/ngx-monaco-editor.component';
+export * from './lib/ngx-monaco-editor.module';
