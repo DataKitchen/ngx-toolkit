f6928434c7ff4175974a543f6d992c20b7a47392
Davide Cavaliere <dcavaliere@datakitchen.io>
2024-01-25 12:52:15 +0100

    chore(ngx-monaco-editor): initial jest config. tests still fail

---
+++ projects/ngx-monaco-editor/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  displayName: 'ngx-monaco-editor',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {},
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 89,
+      branches: 85,
+      lines: 90,
+      functions: 79,
+    },
+  },
+};
